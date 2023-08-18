8a0359c9ca2ef256835aa2ecccddc0e251b53bfb
Sunnatilla Ismoilov <sunnatilla.ismailov@gmail.com>
2023-08-18 15:20:46 +0300

    Did migration for users

--- database/migrations/2023_08_18_104519_users.php
+++ database/migrations/2023_08_18_104519_users.php
@@ -13,7 +13,17 @@ class Users extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('country');
+            $table->string('street_adress');
+            $table->string('town_city');
+            $table->string('zip_code');
+            $table->string('phone');
+            $table->string('email');
+        });
     }
 
     /**
