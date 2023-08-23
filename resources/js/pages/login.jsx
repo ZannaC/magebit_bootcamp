3c3b2c0fdeb36c7cf2deda2e8a088b0238e74474
nesj <alexanmiro@gmail.com>
2023-08-23 11:39:43 +0300

    login page logic

--- resources/js/pages/login.jsx
+++ resources/js/pages/login.jsx
@@ -4,12 +4,30 @@ import "../../sass/app.css";
 
 function Login() {
     const { setLogin, loggedIn } = useProduct();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
     const [response, setResponse] = useState("");
+    // validation states
+    const [noUserError, setNoUserError] = useState(false);
+    const [emptyEmail, setEmptyEmail] = useState(false);
+    const [emptyPassword, setEmptyPassword] = useState(false);
+
+    useEffect(() => {
+        if (email === "") {
+            setEmptyEmail(true);
+        } else {
+            setEmptyEmail(false);
+        }
+        if (password === "") {
+            setEmptyPassword(true);
+        } else {
+            setEmptyPassword(false);
+        }
+    }, [email, password])
 
     const changeEmail = (e) => {
         setEmail(e.target.value);
+        setNoUserError(false);
     };
 
     const changePassword = (e) => {
@@ -26,7 +44,7 @@ function Login() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                // 'X-CSRF-TOKEN': document.querySelector('[name="csrf-token"]').getAttribute('content')
+                'X-CSRF-TOKEN': document.querySelector('[name="csrf-token"]').getAttribute('content')
             },
             body: JSON.stringify(obj),
         })
@@ -40,6 +58,9 @@ function Login() {
     useEffect(() => {
         if (response.userExists === 'est') {
             setLogin();
+        } else if (response.userExists === 'net') {
+            console.log (response)
+            setNoUserError(true);
         }
     }, [response]);
 
@@ -47,16 +68,25 @@ function Login() {
         <section className="login__container">
             <div className="login">
                 <h2>Log in to Avion</h2>
-                <form className="login__form" method="POST">
-                    <input
-                        onChange={changeEmail}
-                        name="email"
-                        type="email"
-                        placeholder="enter your email"
-                        required
-                    />
+                <form className='login__form' method="POST">
+                    <div className="login__form-input-email-block">
+                        <span className={noUserError ? 'login__form-input-email-block-span-error' : 'login__form-input-email-block-span'}>Wrong email</span>
+                        <input
+                            onChange={changeEmail}
+                            className={noUserError ?
+                             'login__form-input-error'
+                             :
+                             'login__form-input'
+                            }
+                            name="email"
+                            type="email"
+                            placeholder="enter your email"
+                            required
+                        />
+                    </div>
                     <input
                         onChange={changePassword}
+                        className='login__form-input'
                         name="password"
                         type="password"
                         placeholder="password"
---
+++ resources/views/search.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input name="title" type="text" placeholder="search book">
+        <button type="submit">submit</button>
+    </form>
+</body>
+</html>
